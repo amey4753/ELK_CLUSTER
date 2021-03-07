f5bf93c5fbb8459153b3d20f1064fbb89f6aaa08
Amey Vikram Singh <41503174+amey4753@users.noreply.github.com>
2021-03-07 16:52:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -115,6 +115,20 @@ resource "aws_security_group" "sg12" {
     protocol  = "-1"
     self      = true
   }
+    
+    
+    ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9300
+    to_port     = 9300
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   
   egress {
     from_port = "0"
