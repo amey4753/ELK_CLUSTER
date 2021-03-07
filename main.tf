ba165147b161a0f96bf929c5b1a3e85251e09e73
Amey Vikram Singh <41503174+amey4753@users.noreply.github.com>
2021-03-07 16:56:02 +0530

    added port for elastic search

--- main.tf
+++ main.tf
@@ -115,6 +115,12 @@ resource "aws_security_group" "sg12" {
     protocol  = "-1"
     self      = true
   }
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
     
     
     ingress {
