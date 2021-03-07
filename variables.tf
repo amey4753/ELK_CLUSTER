4ffd521b0aa39cf3e7029defed214f915c45a9da
Amey Vikram Singh <41503174+amey4753@users.noreply.github.com>
2021-03-07 15:35:00 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,29 @@
+
+variable "region" {
+  description = "AWS Deployment region.."
+  default = "ap-south-1"
+}
+
+variable "environment" {
+  description = "The Deployment environment"
+}
+
+//Networking
+variable "vpc_cidr" {
+  description = "The CIDR block of the vpc"
+}
+
+variable "public_subnets_cidr" {
+  type        = string
+  description = "The CIDR block for the public subnet"
+}
+
+variable "private_subnets_cidr" {
+  type        = string
+  description = "The CIDR block for the private subnet"
+}
+variable "availability_zones" {
+  type        = list
+  description = "The az that the resources will be launched"
+}
+
